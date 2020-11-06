b5a98e9b2c98ff4665398fc85e366f4f6803488d
Bima Saridjo <palessie27@gmail.com>
2020-11-06 11:13:38 -0500

    updates

---
+++ src/imgs/index.js
@@ -0,0 +1,7 @@
+import Fehu from './Fehu.png';
+import Uruz from './Uruz.png';
+import Thurisaz from './Thurisaz.png';
+import Ansuz from './Ansuz.png';
+import Raidho from './Raidho.png';
+
+export { Fehu, Uruz, Thurisaz, Ansuz, Raidho };
