4dbc3f235dc560025133602e4a7e1c44f76d7456
Bima Saridjo <palessie27@gmail.com>
2020-11-11 18:23:54 -0500

    noreferrer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <div className="header">
-      <a href="http://www.sba-nyc.com" target="_blank">
+      <a href="http://www.sba-nyc.com" target="_blank" rel="noreferrer">
         <img src={Logo} alt="SBA Logo"/>
       </a>
     </div>
